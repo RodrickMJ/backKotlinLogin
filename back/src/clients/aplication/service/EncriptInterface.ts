7da49635b652edcac6ba9ff0afee18cda48c6464
RodrickMJ <231199@ids.upchiapas.edu.mx>
2025-07-31 12:33:08 -0600

    cambios clinetes

--- back/src/clients/aplication/service/EncriptInterface.ts
+++
@@ -1,4 +0,0 @@
-export default interface EncriptInterface {
-    hash(password: string): Promise<string>;
-    compare (hash_password: string, plain_password: string): Promise<boolean>;
-} 
--- back/src/clients/aplication/service/TokenInterface.ts
+++
@@ -1,6 +0,0 @@
-
-export default interface TokenInterface {
-    generateToken(id: string): string;
-    validateToken(id: string): boolean;
-    getPayload(token: string): string | null;
-}
